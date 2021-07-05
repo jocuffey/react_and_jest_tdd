a90738b55c2de27d9f40e3558fa2a5a6817590d5
jocuffey <joanne.cuffey@makingwaves.com>
2021-07-05 13:26:15 +0200

    delete unnecessary files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,28 @@
 module.exports = {
-    extends: ['react-app', 'prettier'],
-    plugins: ['prettier', 'jest', 'cypress'],
-    parser: 'babel-eslint',
-    env: {
-      browser: true,
-      'cypress/globals': true,
-      es6: true,
-      'jest/globals': true,
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'jest', 'cypress'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    'cypress/globals': true,
+    es6: true,
+    'jest/globals': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    settings: {
-      react: {
-        version: 'detect',
+  },
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'avoid',
+        bracketSpacing: false,
+        trailingComma: 'all',
+        endOfLine: 'auto',
+        singleQuote: true,
       },
-    },
-    rules: {
-      'prettier/prettier': 'warn',
-    },
-  };
+    ],
+  },
+};
